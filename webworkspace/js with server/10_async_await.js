01a9f55b20e9787089a9807ff9d10096b834329d
jin ◡̈ <alswls7887@gmail.com>
2024-11-11 17:50:04 +0900

    to yedam

--- webworkspace/js with server/10_async_await.js
+++ webworkspace/js with server/10_async_await.js
@@ -1,5 +1,7 @@
 // 10_async_await.js
 
+// 함수 내부는 동기식이 맞음
+//
 async function getPostInfo() {
    let postList = await fetch('https://jsonplaceholder.typicode.com/posts')
                         .then(res => res.json());
@@ -14,4 +16,6 @@ async function getPostInfo() {
    console.log(post)
 }
 
+// 함수자체는 동기식이 맞으나 밖에서 보면 비동기 작업이다.
 console.log(getPostInfo());
+console.log("코드 종료");
