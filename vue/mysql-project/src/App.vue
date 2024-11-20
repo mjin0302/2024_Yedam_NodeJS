255d608967217f16b7cfdce408b1aba374a9850e
jin ◡̈ <alswls7887@gmail.com>
2024-11-20 17:52:02 +0900

    to yedam

--- vue/mysql-project/src/App.vue
+++ vue/mysql-project/src/App.vue
@@ -2,6 +2,8 @@
   <div>
     <!-- 레이아웃 잡기 -->
     <!-- header component -->
+    <DefaultHeader/>
+
     <!-- 링크를 기분으로 라우터 내부에 있는 ~~~를 선택할 수 있다
     어느것을 보여줄지 선택
     선택에 따라서 동적으로 바뀐다 
@@ -9,17 +11,26 @@
     경로와 컴포넌트를 연결하는 작업을 한다 
     내부에 실제 서버가 존재하지 않지만 실제 서버가 존재하는 것 처럼 착각을 일으킴
     라우터를 기반으로 어떻게 데이터를 넘기냐? query, params를 이용해서 가능하디-->
-    <router-link to="/customerList">전체조회</router-link>
-    <router-link to="/customerAdd">등록</router-link>
+    <!-- <router-link to="/customerList">전체조회</router-link>
+    <router-link to="/customerAdd">등록</router-link> -->
 
     <!-- content component -->
     <router-view/>
 
     <!-- footer component -->
+
   </div>
 </template>
 
 <script>
+  import DefaultHeader from './layouts/DefaultHeader.vue';
+
+  export default {
+
+    components : {
+      DefaultHeader
+    }
+  }
 
 </script>
 
---
+++ vue/mysql-project/src/layouts/DefaultHeader.vue
@@ -0,0 +1,17 @@
+<!-- layouts/DefaultHeader.vue -->
+<template>
+   <nav class="nav">
+      <router-link class="nav-link" to="/customerList">전체조회 | </router-link> <!-- router-link태그의 기본은 a태그임 -->
+      <router-link class="nav-link" to="/customerAdd">등록</router-link>
+   </nav>
+</template>
+
+<script>
+   export default {
+      
+   }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
